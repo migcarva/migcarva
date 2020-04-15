6c98604f0d1378817d6cf436ee9ea284ab66800f
Miguel Carvalho <emece.pt@gmail.com>
2020-04-15 23:50:53 +0100

    Add work namespace

---
+++ src/routes/work/_layout.svelte
@@ -0,0 +1,41 @@
+<script>
+	import Menu from '../components/Menu.svelte';
+
+	export let segment;
+</script>
+
+<style>
+	main {
+		width: 100%;
+		padding-right: 15px;
+		padding-left: 15px;
+		margin-right: auto;
+		margin-left: auto;
+	}
+	@media (min-width: 576px) {
+		main {
+			max-width: 540px;
+		}
+	}
+	@media (min-width: 768px) {
+		main {
+			max-width: 720px;
+		}
+	}
+	@media (min-width: 992px) {
+		main {
+			max-width: 960px;
+		}
+	}
+	@media (min-width: 1200px) {
+		main {
+				max-width: 1140px;
+		}
+	}
+</style>
+
+<Menu {segment}/>
+
+<main>
+	<slot></slot>
+</main>
