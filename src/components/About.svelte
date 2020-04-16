eb16a7af8a2d89168c24d41a812fe3ea7d7f0ad3
Miguel Carvalho <emece.pt@gmail.com>
2020-04-16 17:19:26 +0100

    Delete Hero Component

---
+++ src/components/About.svelte
@@ -0,0 +1,37 @@
+<section id="about">
+	<Title {...title} />
+	<div class="row qa justify-content-center">
+	<div class="col-md-2 question qanda"><h3>About us</h3></div>
+	<div class="col-md-5 answer qanda">
+	<p>We are a talented and ambitious young team, able to solve complex problems in a brilliant and innovative way. We explore new technologies with enthusiasm and passion, always pushing ourselves further. We take care of every detail, even the smallest.</p>
+	</div>
+	<div class="col-md-5 answer qanda">
+	<p>Our goal is to create and improve digital products, ensuring maximum usability and crafting an essential and orderly design. Simplicity is our strength: we highlight what really matters and we are really good at it.</p>
+	<a class="link" href="../en/about.html">Discover more</a>
+	</div>
+	</div>
+</section>
+
+<script>
+	import Title from './Title.svelte';
+
+	let title = {
+		main: "Frontend developer from Lisbon",
+		lead: "I like it simple, clean and straightforward",
+		isHero: false,
+	}
+</script>
+
+<style>
+	section {
+		height: 100vh;
+		margin-top: 0;
+		padding-top: 50px;
+		position: relative;
+	}
+	@media (max-width: 767px) {
+		section {
+			padding-top: 20px;
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,11 +2,44 @@
 	<title>migcarva. Welcome</title>
 </svelte:head>
 
-<Hero />
+<section>
+	<Logo />
+	<Title {...heroTitle} />
+	<!-- <a class="freccia" href="#list"></a> -->
+</section>
+
+<section>
+	<Logo />
+	<Title {...heroTitle} />
+	<!-- <a class="freccia" href="#list"></a> -->
+</section>
 
 <script>
-	import Hero from '../components/Hero.svelte';
+	import Title from './Title.svelte';
+	import Logo from './Logo.svelte';
+
+	let heroTitle = {
+		main: "I'm Miguel Carvalho",
+		lead: "I like it simple, clean and straightforward",
+		isHero: true,
+	}
+	let aboutTitle = {
+		main: "I'm Miguel Carvalho",
+		lead: "I like it simple, clean and straightforward",
+		isHero: true,
+	}
 </script>
 
 <style>
+	section {
+		height: 100vh;
+		margin-top: 0;
+		padding-top: 50px;
+		position: relative;
+	}
+	@media (max-width: 767px) {
+		section {
+			padding-top: 20px;
+		}
+	}
 </style>
