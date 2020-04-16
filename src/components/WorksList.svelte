4fc0c5a751f60e4cd4aa9a6cddb1255e86b78c88
Miguel Carvalho <emece.pt@gmail.com>
2020-04-16 20:40:45 +0100

    Add animations to work image and caption

--- src/components/WorksList.svelte
+++ src/components/WorksList.svelte
@@ -4,23 +4,25 @@
 		{#each works as work}
 			<li class="col-12 col-md-6">
 				<a href={`/works/${work.slug}.html`}>
-					<div>
-						<img
-							srcset={`
-								images/works/${work.slug}_cover_home.png 1x,
-								images/works/${work.slug}_cover_home@2x.png 2x
-							`}
-							alt={work.title}
-						>
-					</div>
-					<div class="project__description">
-						<div class="project__title">
-							<h3 class="heading-3">{work.title}</h3>
+					<figure>
+						<div>
+							<img
+								srcset={`
+									images/works/${work.slug}_cover_home.png 1x,
+									images/works/${work.slug}_cover_home@2x.png 2x
+								`}
+								alt={work.title}
+							>
 						</div>
-						<p class="project__paragraph">
-							{work.description}
-						</p>
-					</div>
+						<figcaption>
+							<div>
+								<h3 class="heading-3">{work.title}</h3>
+							</div>
+							<p>
+								{work.description}
+							</p>
+						</figcaption>
+					</figure>
 				</a>
 			</li>
 		{/each}
@@ -45,35 +47,73 @@
 	]
 </script>
 
-<style>
+<style lang="scss">
 	li {
 		font-size: 1rem;
     transition: transform .3s;
-    margin-bottom: 6em;
+		margin-bottom: 6em;
+
+		&:hover {
+			figure {
+				div {
+					transform: scale(0.98);
+				}
+				img {
+					transform: scale(1.06);
+				}
+				figcaption {
+					transform: translateY(-10px);
+				}
+			}
+		}
 	}
 
-	li a,
-	li a:link,
-	li a:visited {
+	a {
 		text-decoration: none;
+
+		&:link,
+		&:visited {
+			text-decoration: none;
+		}
 	}
 
-	li a > div:first-child {
-		width: 100%;
-		overflow: hidden;
-		transition: transform .3s;
-	}
+	figure {
+		margin: 0;
 
-	li a > div:last-child {
-		width: 100%;
-		overflow: hidden;
-		transition: transform .3s;
-	}
+		div {
+			width: 100%;
+			overflow: hidden;
+			transition: transform .3s;
 
-	img {
-		width: 100%;
-		pointer-events: none;
-		user-select: none;
-		transition: transform .3s;
+			img {
+				width: 100%;
+				pointer-events: none;
+				user-select: none;
+				transition: transform .3s;
+			}
+		}
+
+		figcaption {
+			margin: 0 auto;
+			margin-top: 3.2em;
+			transition: transform .3s;
+			max-width: 90%;
+			@media only screen and (min-width: 992px) {
+				max-width: 70%;
+			}
+
+			div {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				text-align: center;
+			}
+
+			p {
+				font-weight: medium;
+				text-align: center;
+				color: #707070;
+			}
+		}
 	}
 </style>
