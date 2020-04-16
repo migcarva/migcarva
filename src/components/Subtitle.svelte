eaa66743b00a1bdefbe4eeac4a7674f1cb2a0456
Miguel Carvalho <emece.pt@gmail.com>
2020-04-16 18:10:29 +0100

    Update subtitle to use slot

--- src/components/Subtitle.svelte
+++ src/components/Subtitle.svelte
@@ -1,10 +1,15 @@
-<h3>
-	{title}
-</h3>
+<div class={col ? `col-md-${col}` : ''}>
+	<h3><slot></slot></h3>
+</div>
 
 <script>
-	export let title;
+	export let col;
 </script>
 
 <style>
+	h3 {
+		font-family: "HelveticaMedium", Arial, sans-serif;
+    font-size: 18px;
+    padding-top: 4px;
+	}
 </style>
