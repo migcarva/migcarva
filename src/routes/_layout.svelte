96ff6f3217cd3609ac19990e95efac65c9b8cf9c
Miguel Carvalho <emece.pt@gmail.com>
2020-04-17 20:02:11 +0100

    Fix laptop

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -53,44 +53,14 @@
 	}
 
 	main {
-		width: 100%;
+		width: 85%;
 		padding-right: 15px;
-		padding-left: 15px;
+		padding-left: 15%;
 		margin-right: auto;
 		margin-left: auto;
 	}
 
-	@media (max-width: 576px) and (orientation: portrait) {
-		main {
-			width: 90%;
-			padding-left: 10%;
-		}
-	}
-
-	@media only screen
-	and (max-width: 320px)
-	and (max-width: 568px)
-	and (-webkit-min-device-pixel-ratio: 2) {
-		main {
-			width: 85%;
-			padding-left: 15%;
-		}
-	}
-
-	@media only screen
-  and (min-width: 375px)
-  and (max-width: 667px)
-  and (-webkit-min-device-pixel-ratio: 2) {
-		main {
-			width: 90%;
-			padding-left: 10%;
-		}
-	}
-
-	@media only screen
-  and (min-width: 768px)
-  and (max-width: 1024px)
-  and (-webkit-min-device-pixel-ratio: 1) {
+	@media (min-width: 768px)  {
 		main {
 			width: 90%;
 			padding-left: 10%;
