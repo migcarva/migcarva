246488d3d112cc226e390f016b1a12151462ea3c
Miguel Carvalho <emece.pt@gmail.com>
2020-04-17 16:01:39 +0100

    Add fly to Vertical Logo based on scroll y position

--- src/components/LogoVertical.svelte
+++ src/components/LogoVertical.svelte
@@ -1,57 +1,57 @@
-<div id="page-title">
-	<h1>
-		<a href="/">migcarva.</a>
-	</h1>
-</div>
+<svelte:window bind:scrollY={y}/>
+
+{#if y > 800}
+	<div transition:fly="{{ y: -20, duration: 500 }}">
+		<h1>
+			<a href="/">migcarva.</a>
+		</h1>
+	</div>
+{/if}
 
 <script>
+	import { fade, fly } from 'svelte/transition';
+	let y;
 </script>
 
-<style>
+<style lang="scss">
 	div {
-		font-family: var(--font);
-		font-weight: 600;
-		font-size: 16px;
 		position: fixed;
 		z-index: 10;
 		margin: 0 0 0 0;
 		padding: 0;
-		text-align: right;
 		top: 80px;
-		left: -16px;
+		left: -8px;
 		transform: rotate(-90deg) translate(0, 0);
 		transition: transform 0.3s cubic-bezier(0.6, 0.2, 0.1, 1);
-	}
-
-	div a {
-    font-family: var(--font);
-    font-size: 18px;
-	}
-
-	/* div a.contact span {
-		font-style: italic;
-	} */
-
-	/* div.hidden {
-		transform: rotate(-90deg) translate(0, -100px);
-		transition: transform 0.2s cubic-bezier(0.6, 0.2, 0.1, 1);
-	} */
-
-	/* div span {
-			color: #b0b0b0;
-	} */
-
-	div:hover{
-		transform: rotate(-90deg) translate(0, 0) scale(0.95);
-		transition: transform 0.2s ease-out;
-	}
-
-	@media only screen and (max-height: 500px) and (orientation: landscape) and (-webkit-min-device-pixel-ratio: 2) {
-		div {
+		@media only screen
+			and (max-height: 500px)
+			and (orientation: landscape)
+			and (-webkit-min-device-pixel-ratio: 2) {
 			left: -11px;
 		}
-		div a {
-			padding-left: env(safe-area-inset-left);
+
+		// &.isHidden {
+		// 	transform: rotate(-90deg) translate(0, -100px);
+		// 	transition: transform 0.2s cubic-bezier(0.6, 0.2, 0.1, 1);
+		// }
+
+		h1 {
+			font-family: var(--font);
+			font-weight: var(--font-bold);
+			font-size: var(--font-base);
+			text-align: right;
+
+			a {
+				font-family: var(--font);
+				font-size: var(--font-base);
+
+				@media only screen
+					and (max-height: 500px)
+					and (orientation: landscape)
+					and (-webkit-min-device-pixel-ratio: 2) {
+						padding-left: env(safe-area-inset-left);
+				}
+			}
 		}
 	}
 </style>
