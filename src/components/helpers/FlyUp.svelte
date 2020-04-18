bfaaaf415eafb59e46d9b856990a1cc811da023a
Miguel Carvalho <emece.pt@gmail.com>
2020-04-18 18:38:48 +0100

    Add Intersection Observer and FlyUp helper

---
+++ src/components/helpers/FlyUp.svelte
@@ -0,0 +1,12 @@
+<Intersection once let:intersecting top={0}>
+	{#if intersecting}
+    <div transition:fly="{{ y: 500, duration: 1000, delay: 300 }}">
+		  <slot></slot>
+    </div>
+	{/if}
+</Intersection>
+
+<script>
+	import { fly } from 'svelte/transition';
+	import Intersection from '../observers/Intersection.svelte';
+</script>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,33 +4,39 @@
 
 <section id="hero">
 	<Logo />
-	<h2>
-		I'm Miguel Carvalho<br>
-		<span>— Perpetually curious and forever learning</span>
-	</h2>
+	<FlyUp>
+		<h2>
+			I'm Miguel Carvalho<br>
+			<span>— Perpetually curious and forever learning</span>
+		</h2>
+	</FlyUp>
 	<!-- <a class="freccia" href="#list"></a> -->
 </section>
 
 <section id="about">
-	<h2>
-		A frontend developer from Lisbon<br>
-		<span>— I enjoy simple, concise and readable code</span>
-	</h2>
-	<div class="row qa justify-content-center">
-		<div class="col-2">
-			<h3>About me</h3>
+	<FlyUp>
+		<h2>
+			A frontend developer from Lisbon<br>
+			<span>— I enjoy simple, concise and readable code</span>
+		</h2>
+	</FlyUp>
+	<FlyUp>
+		<div class="row 5a justify-content-center">
+			<div class="col-2">
+				<h3>About me</h3>
+			</div>
+			<div class="col-5">
+				<p>
+					I started doing frontend development a long time ago, when it was my hobby and I was studying to become an architect. I spend a lot of my time programming. Not just for work, but as a general hobby. I have a strong passion for learning new things, and I’m always taking time out of my day to pick up something new.
+				</p>
+			</div>
+			<div class="col-5">
+				<p>
+					I'm currently workin as a Senior Frontend at Unbabel, where I'm part of the Language Operations Team, a crucial team supporting Unbabel's clients. I spent most of my time reviewing code, mentoring younger developers and chilling out in the caffetaria.
+				</p>
+			</div>
 		</div>
-		<div class="col-5">
-			<p>
-				I started doing frontend development a long time ago, when it was my hobby and I was studying to become an architect. I spend a lot of my time programming. Not just for work, but as a general hobby. I have a strong passion for learning new things, and I’m always taking time out of my day to pick up something new.
-			</p>
-		</div>
-		<div class="col-5">
-			<p>
-				I'm currently workin as a Senior Frontend at Unbabel, where I'm part of the Language Operations Team, a crucial team supporting Unbabel's clients. I spent most of my time reviewing code, mentoring younger developers and chilling out in the caffetaria.
-			</p>
-		</div>
-	</div>
+	</FlyUp>
 	<a class="action" href="../en/about.html">Discover more</a>
 </section>
 
@@ -39,6 +45,7 @@
 <script>
 	import Logo from '../components/Logo.svelte';
 	import WorksList from '../components/WorksList.svelte';
+	import FlyUp from '../components/helpers/FlyUp.svelte';
 </script>
 
 <style lang="scss">
