4f775036347a3346b1d01c79aab6eedfcddc7356
Miguel Carvalho <emece.pt@gmail.com>
2020-04-21 18:49:39 +0100

    Add works.json

---
+++ src/routes/works/[slug].svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>migcarva. Work</title>
+</svelte:head>
+
+
+
+<script>
+	// export let work;
+</script>
+
+<style>
+</style>
---
+++ src/routes/works/_works.js
@@ -0,0 +1,292 @@
+const works = [
+	// portugaldesigual
+	{
+		slug: 'portugaldesigual',
+		title: 'Portugal Desigual',
+		url: 'https://portugaldesigual.ffms.pt/',
+		client: {
+			name: 'Fundação Francisco Manuel dos Santos',
+			url: 'https://ffms.pt/',
+		},
+		employer: {
+			name: 'East Atlantic Engineering',
+			url: 'https://eae.pt',
+		},
+		tech: 'Vue.js',
+		punchline: 'Lorem ipsum dolor sit amet',
+		description: `
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		`,
+		thumbnail: {
+			x1: '',
+			x2: '',
+		},
+		cover: {
+			x1: '',
+			x2: '',
+		},
+		images: [
+			{
+				x1: '',
+				x2: '',
+			}
+		],
+	},
+
+	//generation M
+	{
+		slug: 'generation-m',
+		title: 'Migros Generation M',
+		url: 'https://generation-m.migros.ch/de.html',
+		client: {
+			name: 'Migros',
+			url: 'https://www.migros.ch/de.html',
+		},
+		employer: {
+			name: 'East Atlantic Engineering',
+			url: 'https://eae.pt',
+		},
+		tech: 'Vanila JS',
+		punchline: 'Lorem ipsum dolor sit amet',
+		description: `
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		`,
+		thumbnail: {
+			x1: '',
+			x2: '',
+		},
+		cover: {
+			x1: '',
+			x2: '',
+		},
+		images: [
+			{
+				x1: '',
+				x2: '',
+			}
+		],
+	},
+
+	//generation M
+	{
+		slug: 'theke',
+		title: 'Migros Theke',
+		url: 'https://theke.migros.ch/de',
+		client: {
+			name: 'Migros',
+			url: 'https://www.migros.ch/de.html',
+		},
+		employer: {
+			name: 'East Atlantic Engineering',
+			url: 'https://eae.pt',
+		},
+		tech: 'Laravel + Blade',
+		punchline: 'Lorem ipsum dolor sit amet',
+		description: `
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		`,
+		thumbnail: {
+			x1: '',
+			x2: '',
+		},
+		cover: {
+			x1: '',
+			x2: '',
+		},
+		images: [
+			{
+				x1: '',
+				x2: '',
+			}
+		],
+	},
+
+	//Station
+	{
+		slug: 'station',
+		title: 'Station',
+		url: 'https://station.ch/de',
+		client: {
+			name: 'Station',
+			url: 'https://station.ch/de',
+		},
+		employer: {
+			name: 'East Atlantic Engineering',
+			url: 'https://eae.pt',
+		},
+		tech: 'Drupal',
+		punchline: 'Lorem ipsum dolor sit amet',
+		description: `
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		`,
+		thumbnail: {
+			x1: '',
+			x2: '',
+		},
+		cover: {
+			x1: '',
+			x2: '',
+		},
+		images: [
+			{
+				x1: '',
+				x2: '',
+			}
+		],
+	},
+
+	//Station
+	{
+		slug: 'educacao-em-exame',
+		title: 'Educação em Exame',
+		url: 'https://educacaoemexame.pt/',
+		client: {
+			name: 'Fundação Francisco Manuel dos Santos',
+			url: 'https://ffms.pt/',
+		},
+		employer: {
+			name: 'East Atlantic Engineering',
+			url: 'https://eae.pt',
+		},
+		tech: 'Vue.js',
+		punchline: 'Lorem ipsum dolor sit amet',
+		description: `
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		`,
+		thumbnail: {
+			x1: '',
+			x2: '',
+		},
+		cover: {
+			x1: '',
+			x2: '',
+		},
+		images: [
+			{
+				x1: '',
+				x2: '',
+			}
+		],
+	},
+
+	//Serious
+	{
+		slug: 'serious',
+		title: 'Serious Work',
+		url: 'https://serious.work',
+		client: {
+			name: 'Serious Work',
+			url: 'https://serious.work',
+		},
+		employer: {
+			name: 'East Atlantic Engineering',
+			url: 'https://eae.pt',
+		},
+		tech: 'Vue.js',
+		punchline: 'Lorem ipsum dolor sit amet',
+		description: `
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		`,
+		thumbnail: {
+			x1: '',
+			x2: '',
+		},
+		cover: {
+			x1: '',
+			x2: '',
+		},
+		images: [
+			{
+				x1: '',
+				x2: '',
+			}
+		],
+	},
+
+	//BPStat
+	{
+		slug: 'bpstat',
+		title: 'Bank of Portugal Stats',
+		url: 'https://bpstat.bportugal.pt',
+		client: {
+			name: 'Bank of Portugal',
+			url: 'https://bportugal.pt',
+		},
+		employer: {
+			name: 'Bright Pixel',
+			url: 'https://brpx.com/',
+		},
+		tech: 'Django + React.js',
+		punchline: 'Lorem ipsum dolor sit amet',
+		description: `
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		`,
+		thumbnail: {
+			x1: '',
+			x2: '',
+		},
+		cover: {
+			x1: '',
+			x2: '',
+		},
+		images: [
+			{
+				x1: '',
+				x2: '',
+			}
+		],
+	},
+
+	//brpx
+	{
+		slug: 'brpx',
+		title: 'Bright Pixel',
+		url: 'https://brpx.com/',
+		client: {
+			name: 'Bright Pixel',
+			url: 'https://brpx.com/',
+		},
+		employer: {
+			name: 'Bright Pixel',
+			url: 'https://brpx.com/',
+		},
+		tech: 'React.js',
+		punchline: 'Lorem ipsum dolor sit amet',
+		description: `
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		`,
+		thumbnail: {
+			x1: '',
+			x2: '',
+		},
+		cover: {
+			x1: '',
+			x2: '',
+		},
+		images: [
+			{
+				x1: '',
+				x2: '',
+			}
+		],
+	},
+];
+
+/*
+https://bpstat.bportugal.pt
+https://brpx.com/
+*/
+
+works.forEach(work => {
+	work.html = work.html.replace(/^\t{3}/gm, '');
+});
+
+export default works;
