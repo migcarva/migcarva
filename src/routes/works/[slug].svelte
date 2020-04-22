acb38703873319fac19dbb70d6be68f35f09c7c3
Miguel Carvalho <emece.pt@gmail.com>
2020-04-22 19:29:36 +0100

    Works details

--- src/routes/works/[slug].svelte
+++ src/routes/works/[slug].svelte
@@ -2,11 +2,11 @@
 	<title>migcarva. {work.title}</title>
 </svelte:head>
 
-<div class="hero">
+<div id="hero" class="hero">
 	<figure>
 		<div>
 			<IntersectableTransition
-				anchor="#{work.slug}" threshold={.1}
+				anchor="#hero" threshold={.1}
 				options={{ y: 1280, duration: 1280, delay: 0}}
 			>
 				<img
@@ -17,7 +17,7 @@
 		</div>
 		<figcaption>
 			<IntersectableTransition
-				anchor="#{work.slug}" threshold={.1} isFade
+				anchor="#hero" threshold={.1} isFade
 				options={{ duration: 1920, delay: 1024}}
 			>
 				<h2>{work.title}</h2>
@@ -34,10 +34,57 @@
 	</figure>
 </div>
 
-<section id="{work.slug}" class="container">
-	<div>
-		yo
+<section id="details" class="container">
+	<div class="row">
+		<div class="col-6">
+			<div class="row">
+				<div class="col-6 group">
+					<IntersectableTransition
+						anchor="#details" threshold={.75}
+						options={{ y: 128, duration: 1280, delay: 0}}
+					>
+						<p class="title">CLIENT</p>
+						<a href="{work.client.url}">{work.client.name}</a>
+					</IntersectableTransition>
+				</div>
+				<div class="col-6 group">
+					<IntersectableTransition
+						anchor="#details" threshold={.75}
+						options={{ y: 128, duration: 1280, delay: 256}}
+					>
+						<p class="title">EMPLOYER</p>
+						<a href="{work.employer.url}">{work.employer.name}</a>
+					</IntersectableTransition>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-6 group">
+					<IntersectableTransition
+						anchor="#details" threshold={.75}
+						options={{ y: 128, duration: 1280, delay: 512}}
+					>
+						<p class="title">TECHNOLOGY</p>
+						<p>{work.tech}</p>
+					</IntersectableTransition>
+				</div>
+			</div>
+		</div>
+		<div class="col-6">
+			<IntersectableTransition
+				anchor="#details" threshold={.75}
+				options={{ y: 128, duration: 1280, delay: 640}}
+			>
+				{@html work.description}
+			</IntersectableTransition>
+		</div>
+
 	</div>
+</section>
+
+<section id="galery" class="container">
+	<IntersectableTransition>
+		<h3>{work.punchline}</h3>
+	</IntersectableTransition>
 </section>
 
 <script context="module">
@@ -62,120 +109,131 @@
 </script>
 
 <style lang="scss">
-.hero {
-	position: relative;
-	width: 100%;
-	height: 75vh;
+	.hero {
+		position: relative;
+		width: 100%;
+		height: 75vh;
 
-	figure {
-		position: absolute;
-		top: 0;
-		left: 0;
-		bottom: 0;
-		right: 0;
-
-		> div {
-			width: 100%;
-			height: 100%;
-			overflow: hidden;
-			margin-bottom: 1.2em;
-			position: relative;
-
-			img {
-				width: 100%;
-				height: 100%;
-				object-fit: cover;
-				pointer-events: none;
-				user-select: none;
-				transition: transform .6s cubic-bezier(0, 0.55, 0.45, 1);
-			}
-
-			.hover {
-				width: 100%;
-				position: absolute;
-				top: 50%;
-				left: 50%;
-				text-align: center;
-				transform: translate(-50%, -50%);
-				color: white;
-				font-size: 3em;
-				font-weight: var(--font-medium);
-				text-shadow: 0 4px 4px var(--grey-light);
-				opacity: 0;
-				transition: opacity .3s cubic-bezier(0, 0.55, 0.45, 1);
-			}
-		}
-
-		figcaption {
+		figure {
 			position: absolute;
 			top: 0;
-			width: 100%;
-			height: 100%;
+			left: 0;
+			bottom: 0;
+			right: 0;
 
-			:global(> div div) {
+			> div {
 				width: 100%;
 				height: 100%;
-				display: flex;
-				flex-direction: column;
-				justify-content: center;
-				align-items: center;
-			}
-
-			h2 {
-				color: white;
-				font-size: calc(var(--font-h2) * 1.25);
-				margin-bottom: 1rem;
-			}
-
-			a {
+				overflow: hidden;
+				margin-bottom: 1.2em;
 				position: relative;
 
-				&:hover {
-					.left, .right {
-						width: 45%;
-					}
-				}
-
-				p {
-					color: white;
-					font-size: calc(var(--font-h3) * 1.5);
-					margin-bottom: 0;
-				}
-
-				div {
-					position: relative;
+				img {
 					width: 100%;
-					height: 2px;
+					height: 100%;
+					object-fit: cover;
+					pointer-events: none;
+					user-select: none;
+					transition: transform .6s cubic-bezier(0, 0.55, 0.45, 1);
+				}
+			}
+
+			figcaption {
+				position: absolute;
+				top: 0;
+				width: 100%;
+				height: 100%;
+
+				:global(> div div) {
+					width: 100%;
+					height: 100%;
+					display: flex;
+					flex-direction: column;
+					justify-content: center;
+					align-items: center;
+				}
+
+				h2 {
+					color: white;
+					font-size: calc(var(--font-h2) * 1.25);
+					margin-bottom: 1rem;
+				}
+
+				a {
+					position: relative;
+
+					&:hover {
+						.left, .right {
+							width: 45%;
+						}
+					}
+
+					p {
+						color: white;
+						font-size: calc(var(--font-h3) * 1.5);
+						margin-bottom: 0;
+					}
 
 					div {
-						display: flex;
+						position: relative;
+						width: 100%;
 						height: 2px;
-						background: white;
-						top: 0;
-						transition: width .25s ease-in;
 
-						&.center {
-							max-width: 24%;
-						}
-
-						&.left, &.right {
-							position: absolute;
+						div {
+							display: flex;
+							height: 2px;
+							background: white;
 							top: 0;
-							width: 0;
-						}
+							transition: width .25s ease-in;
 
-						&.left {
-							left: 0;
-							// animation: xpandAndContractLeft .25s linear 0 1 alternate forwards;
-						}
+							&.center {
+								max-width: 24%;
+							}
 
-						&.right {
-							right: 0;
+							&.left, &.right {
+								position: absolute;
+								top: 0;
+								width: 0;
+							}
+
+							&.left {
+								left: 0;
+								// animation: xpandAndContractLeft .25s linear 0 1 alternate forwards;
+							}
+
+							&.right {
+								right: 0;
+							}
 						}
 					}
 				}
 			}
 		}
 	}
-}
+
+	#details {
+		margin-top: 10vh;
+		min-height: 30vh;
+
+		.group {
+			margin-bottom: 2rem;
+
+			p, a {
+				font-weight: 600;
+			}
+
+			.title {
+				margin-bottom: 0;
+				font-weight: 300;
+			}
+		}
+
+	}
+
+	#galery {
+		margin-top: 15vh;
+		h3 {
+			font-size: calc(var(--font-h2) * .85);
+		}
+	}
 </style>
