33caad292751233355e84bb78576495fb15dee08
Miguel Carvalho <emece.pt@gmail.com>
2020-04-23 11:09:23 +0100

    Remove replace function  in posts.js

--- src/routes/blog/_posts.js
+++ src/routes/blog/_posts.js
@@ -1,12 +1,3 @@
-// Ordinarily, you'd generate this data from markdown files in your
-// repo, or fetch them from a database of some kind. But in order to
-// avoid unnecessary dependencies in the starter template, and in the
-// service of obviousness, we're just going to leave it here.
-
-// This file is called `_posts.js` rather than `posts.js`, because
-// we don't want to create an `/blog/posts` route — the leading
-// underscore tells Sapper not to do that.
-
 const posts = [
 	{
 		title: 'What is Sapper?',
@@ -89,9 +80,5 @@ const posts = [
 		`
 	}
 ];
-
-posts.forEach(post => {
-	post.html = post.html.replace(/^\t{3}/gm, '');
-});
 
 export default posts;
