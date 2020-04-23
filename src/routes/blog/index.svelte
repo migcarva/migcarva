f7bebeb2dd051cb7ce3d09f836672ef30bbd0200
Miguel Carvalho <emece.pt@gmail.com>
2020-04-23 13:20:27 +0100

    debugging blog

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -14,10 +14,6 @@
 
 		<ul>
 			{#each posts as post}
-				<!-- we're using the non-standard `rel=prefetch` attribute to
-						tell Sapper to load the data for the page as soon as
-						the user hovers over the link or taps it, instead of
-						waiting for the 'click' event -->
 				<li id="{post.slug}">
 					<IntersectableTransition anchor="#{post.slug}" threshold={1}>
 						<a rel='prefetch' href='blog/{post.slug}'>
@@ -50,6 +46,7 @@
 	import Logo from '../../components/Logo.svelte';
 
 	export let posts;
+			console.log(posts);
 </script>
 
 <style lang="scss">
