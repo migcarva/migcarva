53ed6b553979ad2b27a54c497eec44e88426ea5c
Miguel Carvalho <emece.pt@gmail.com>
2020-04-28 12:04:22 +0100

    works summaries update

--- src/routes/works/_works.js
+++ src/routes/works/_works.js
@@ -13,11 +13,11 @@ const works = [
 			name: 'Bright Pixel',
 			url: 'https://brpx.com/',
 		},
-		tech: 'React.js',
-		punchline: 'Lorem ipsum dolor sit amet',
+		tech: 'Vanila JS',
+		punchline: 'Experience, creative and investment',
 		description: `
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>As part of the Labs team, at Bright Pixel, everyone was in charge of maintaing the company's website in good shape. Updating it with new information, creating new content, adding new job oppenings and bug fixing were weekly tasks.</p>
+			<p>No frameworks were used to create this interesting website, neither any animal was killed! All the code was carefully handmade for a prefect control over the dom and it's elements. In my opinion, it shows the commitment and effort the team behind gave while building it.</p>
 		`,
 		images: [
 			{
@@ -53,7 +53,6 @@ const works = [
 		],
 	},
 
-
 	// serious
 	{
 		isSelected: true,
@@ -68,11 +67,11 @@ const works = [
 			name: 'East Atlantic Engineering',
 			url: 'https://eae.pt',
 		},
-		tech: 'Vue.js',
-		punchline: 'Lorem ipsum dolor sit amet',
+		tech: 'Nuxt.js',
+		punchline: 'Serious people doing serious work',
 		description: `
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Serious is an agile tech agency based both in Zurich and Lisbon. They develop high-quality websites, apps and platforms at affordable prices and high quality standards.</p>
+			<p>After a long period using Vue.js, the development team thought it was a good idea to test out Nuxt.js, the Vue alternative to React's Next.js. The development experience was as expected, making us able to deliver a good quality website in record time. All the background animations were handmade!</p>
 		`,
 		images: [
 			{
@@ -117,10 +116,10 @@ const works = [
 			url: 'https://brpx.com/',
 		},
 		tech: 'Django + React.js',
-		punchline: 'Lorem ipsum dolor sit amet',
+		punchline: 'All the stats you need',
 		description: `
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<p>BPstat is an online statistical dissemination website that provides a large volume of statistical data and information. With BPstat, users can easily and efficiently access the statistical series produced by Banco de Portugal, as well as the main economic statistics and indicators produced by other institutions.</p>
+		<p>Although not the pretiest website I've made, the system complexity was really high. The biggest part of the website is only available on the Bank's local network, where the bank employees can add, update and create new data, using the newly created admin system. Is this backoffice that let them manage all the data comming from several diferrent sources, centralizing everything in a integrated and conflit aware system.</p>
 		`,
 		images: [
 			{
@@ -166,10 +165,10 @@ const works = [
 			url: 'https://eae.pt',
 		},
 		tech: 'Drupal',
-		punchline: 'Lorem ipsum dolor sit amet',
+		punchline: 'Amazing agency from Zurich',
 		description: `
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Station is passionate about new technologies and digital communication. They shape the state of the art with their applications, branding and content – and they always take things a step further with smart ideas and innovative use of content, design and technology.</p>
+			<p>This website was made using Drupal, which was a challenge for me. All the moving parts were a bit hidden behid the framework curtins, and you'd need to open a lot of doors before finding what you're looking for. Also, customizing drupal is no easy job, but when done right allows you to achieve impressive work!</p>
 		`,
 		images: [
 			{
@@ -219,11 +218,11 @@ const works = [
 			name: 'East Atlantic Engineering',
 			url: 'https://eae.pt',
 		},
-		tech: 'Vue.js',
-		punchline: 'Lorem ipsum dolor sit amet',
+		tech: 'Vue.js + Highcharts',
+		punchline: 'How good is the Portuguese education system?',
 		description: `
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Educação em Exame, is the result of a study led by Francisco Manuel dos Santos foundation and Expresso newspapper, to give insigths about the evolution and quality of the Portuguese education system, with the collaboration of the Portuguese National Council for Education.</p>
+			<p>Showing data in a compeling way is always a challenging job. On the other hand, this was a website created to publish the result of a long study and feedback gathered from the target audience, while also providing a way to get more detailed insights through a questionaire directed at students.</p>
 		`,
 		images: [
 			{
@@ -268,11 +267,11 @@ const works = [
 			name: 'East Atlantic Engineering',
 			url: 'https://eae.pt',
 		},
-		tech: 'Vue.js',
-		punchline: 'Lorem ipsum dolor sit amet',
+		tech: 'jQuery + Highcharts',
+		punchline: 'Inequality after the global crisis',
 		description: `
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Another study from Francisco Manuel dos Santos Foundation, analysing the impact of the Euro Crisis in the portuguese society.</p>
+			<p>Creating interesting interactive infocharts is always a technical challenge, but with Highcharts the difficulties are reduce to something very bearable. This was one of the last website I've made using jQuery, and I can't say I miss it a lot. It served me well, though.</p>
 		`,
 		images: [
 			{
@@ -322,11 +321,11 @@ const works = [
 			name: 'East Atlantic Engineering',
 			url: 'https://eae.pt',
 		},
-		tech: 'Laravel + Blade',
-		punchline: 'Lorem ipsum dolor sit amet',
+		tech: 'Laravel + jQuery',
+		punchline: 'Having a party? Order all online!',
 		description: `
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Migros asked us to build this special marketplace for people with a bussy life. Having a BBQ at your house next weekend? Going to the park with some friends? Order online and pick your food prepared at the closest store!</p>
+			<p>Migro wanted to create a intereseting experience to choose ingredients. We explore different approaches and explored new ways to order online.</p>
 		`,
 		images: [
 			{
@@ -370,11 +369,11 @@ const works = [
 			name: 'East Atlantic Engineering',
 			url: 'https://eae.pt',
 		},
-		tech: 'Vanila JS',
-		punchline: 'Lorem ipsum dolor sit amet',
+		tech: 'Backbone + jQuery',
+		punchline: "Migros' timeline of special events",
 		description: `
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Another project for Migros, this time a special project to celebrate their anniversary. A Timeline with special events, and several independent small pages with details.</p>
+			<p>Parallax backgrounds, smooth animations, tweened infographics. All powered by jQuery, GSAP and ScrollMagic. Interesting interactive graphics and a lot of cool content to show off.</p>
 		`,
 		images: [
 			{
