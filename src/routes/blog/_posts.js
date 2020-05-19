f31519423f396ef5233f8378c87c919a5b88d636
Miguel Carvalho <emece.pt@gmail.com>
2020-05-19 16:24:32 +0100

    Update post 1

--- src/routes/blog/_posts.js
+++ src/routes/blog/_posts.js
@@ -1,10 +1,12 @@
 const posts = [
 	{
-		title: 'post 001',
-		slug: 'post-001',
-		summary: `Initial Commit`,
+		title: 'Initial Commit',
+		slug: 'initial-commit',
+		summary: `I want to have time to write.`,
 		html: `
-			<p>So this is my first post... So this is my first post...So this is my first post...So this is my first post...So this is my first post...So this is my first post...So this is my first post...So this is my first post...So this is my first post...So this is my first post...So this is my first post...So this is my first post...So this is my first post...So this is my first post...So this is my first post...So this is my first post...So this is my first post...So this is my first post...So this is my first post...So this is my first post...So this is my first post......</p>
+			<p>
+				Well, to be fair, I do have time to write but I don't do it because I spend to much time doing other stuff. I'll try to organize my time more efficiently in the future...
+			</p>
 		`
 	}
 ];
