5f1c85f186e5aac7be6b6877052309cb8038484f
Miguel Carvalho <emece.pt@gmail.com>
2021-02-13 12:37:50 +0000

    Fix og metatag image path and portugal desigual thumb

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -77,19 +77,6 @@
 	import ActionLink from '../components/ActionLink.svelte';
 	import IntersectableTransition from '../helpers/IntersectableTransition.svelte';
 
-	// import { v4 as uuidv4 } from 'uuid';
-	// import { Mixpanel } from '../mixpanel.js'; 
-
-	// Mixpanel.track('Homepage hit');
-	// Mixpanel.track("Video play", {"genre": "hip-hop", "duration in seconds": 42});
-
-	// const USER_ID = uuidv4();
-	// Mixpanel.identify(USER_ID);
-
-	// Mixpanel.people.set({
-	// 	"USER_ID": USER_ID,    // use human-readable names
-	// });
-
   export let works;
 
 	let showScrollText = false;
